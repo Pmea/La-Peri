2e83fb686e0126b0bda2dab0fcbac3ebaa65e503
pmea <mahe.pierre@live.fr>
2015-04-13 23:08:36 +0200

    ajout lib pour nRF24

--- src/arduino_sensor/arduino_sensor.ino
+++ src/arduino_sensor/arduino_sensor.ino
@@ -1,61 +1,44 @@
 #include <SimpleTimer.h>
 
 int DELAY= 1000;            //une seconde
-const int size_buff= 16;
 
 SimpleTimer timerSend;
 
 int photocellPin= 0;        // d'apres les schemas de Fritzing
 int temperaturePin= 1;
 
-int valTemp[size_buff];
-int valLight[size_buff];
-int compt= 0;
+int valTemp;
+int valLight;
 
-unsigned long /*long*/ timeStamp=0;   // ulong: 2^32= 13,8 ans si DELAY= 100 ms
-                                      // ulong long: 2^64= 60 milliard d'an si DELAY= 100ms
+unsigned long timeStamp=0;   // ulong: 2^32= 13,8 ans si DELAY= 100 ms 138 ans si DELAY= 1s
 
 
 void setup(void){
   Serial.begin(9600);
-  timerSend.setInterval(DELAY * 60, sendData);      // buff pour eviter d'envoyer tous le temps
+  timerSend.setInterval(DELAY, sendData);    
 }
 
 
 void loop(void){
   timerSend.run();
-  
-  delay(DELAY);
 }
 
 
-
-
 void sendData(void){
+  Serial.println(millis());
+  
   
   //recuperation valeurs capteurs
-   int tempTmp= analogRead(temperaturePin);       
-  /*float volatage= (tempsTmp * 5.0) / 1024.0;        // a faire dans le serveur si on veut faire donner le choix de degre F/C
-  valTemp[compt]= (int) (voltage - 0.5) * 100;*/
+  valTemp=  analogRead(temperaturePin);       
+  valLight= analogRead(photocellPin);
   
-  valTemp[compt]= tempTmp;
-  valLight[compt]= analogRead(photocellPin);
+  Serial.println(valTemp);    //debbug
+  Serial.println(valLight);
   
-  compt++;
-  
-  if(compt >= size_buff){
-     // commencer a emettre
-    //send timeStamp
-    //du nombre de valeur 
-    for(int i=0; i<compt; i++){
-       // envoyer par ondes radio pour temperature
-    }
-    //arreter d'emettre
+   // commencer a emettre
+   //send timeStamp
+   // envoyer par ondes temperature et lumiere 
+   //arreter d'emettre
       
-    compt=0;
-  }
-
-
-
   timeStamp+=1;
 }
