54b27d68e40e23987f3cdfc08e85290f6c1cb915
Pmea <mahe.pierre@live.fr>
2015-04-20 19:46:50 +0200

    finalisation avec alumage de led et timestamp d'ini'

--- src/arduino_sensor/arduino_sensor.ino
+++ src/arduino_sensor/arduino_sensor.ino
@@ -15,60 +15,88 @@ RF24 radio = RF24(RADIO_CE_PIN, RADIO_CS_PIN);
 
 
 /* sensor */
-int DELAY= 1000;    
+int DELAY= 10000;    
 
+int ledPin= 13;
 int photocellPin= 0;       
 int temperatureCellPin= 1;
 
+
+typedef struct t_payload{
+        uint16_t cmd;
+	uint16_t timeStamp;
+	uint16_t valLight;
+	uint16_t valTemperature;
+	uint16_t valHumidity;
+} payload;
 
 
 SimpleTimer timerSend;
 
 payload data;
-unsigned long timeStamp=0;   // ulong: 2^32= 13,8 ans si DELAY= 100 ms 138 ans si DELAY= 1s
+payload receiv;
 
+unsigned long timeStamp=0;   // ulong: 2^32= 13,8 ans si DELAY= 100 ms 138 ans si DELAY= 1s
 
 
 void setup(void){
   Serial.begin(9600);
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
+  pinMode(13, OUTPUT);
+
   
   radio.begin();
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_1MBPS);
+  radio.openWritingPipe(   0x0000000001LL);
+  radio.openReadingPipe(1, 0x0000000002LL);
   
-  radio.openWritingPipe(0x0000000001LL);
-  radio.openReadingPipe(1, 0xF0F0F0F0D2LL);
+   radio.startListening();
+  
+  /// pour recuperer le time de l'ordinateur
+   data.cmd= INIT;
+   data.timeStamp= 1;
+   data.valTemperature= 2;       
+   data.valLight= 3;
+   data.valHumidity= 4;
 
   
-  radio.startListening();
-  /// pour recuperer le time de l'ordinateur
-  /*
-  while(!radio.available()){}   // on attend que ca soit possible 
-  
-  radio.read(&data, sizeof(Payload));
-  
-  radio.stopListening();
-  */
-  
-  timerSend.setInterval(DELAY, sendData);
-  data.timeStamp=0;
+   radio.stopListening();
+   radio.write(&data, sizeof(payload));
+   radio.startListening();
+   Serial.println("Ecriture");
+   
+   while(!radio.available()){
+   }
+   
+   radio.read(&data, sizeof(payload));
+   Serial.println("Lecutre");
+   
+   timerSend.setInterval(DELAY, sendData);
 }
 
 
 void loop(void){
   timerSend.run();
+  if(radio.available()){
+    radio.read(&receiv, sizeof(payload));
+    if(receiv.cmd == LEDON)
+      digitalWrite(ledPin, HIGH);
+    if(receiv.cmd == LEDOFF)
+      digitalWrite(ledPin, LOW);
+  }
 }
 
 
 void sendData(void){
   //recuperation valeurs capteurs
+  data.cmd= DATA;
   data.valTemperature= analogRead(temperatureCellPin);       
   data.valLight= analogRead(photocellPin);
   data.valHumidity= 0;
   
-  Serial.println(data.timeStamp);
+   Serial.println(data.timeStamp);
   Serial.println(data.valTemperature);    //debbug
   Serial.println(data.valLight);
   
--- src/arduino_sensor/config.h
+++ src/arduino_sensor/config.h
@@ -1,13 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-
-typedef struct t_payload{
-	unsigned long timeStamp;
-	int16_t valLight;
-	int16_t valTemperature;
-	int16_t valHumidity;
-} payload;
+#define INIT 10
+#define DATA 20
+#define LEDON 30
+#define LEDOFF 40
 
 
 #endif CONFIG_H
